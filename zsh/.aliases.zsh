cf85b6e912836c6e1d39ef20ddd5f7a3792d8502
George Barr <110062762+georgebarr@users.noreply.github.com>
2025-12-29 12:39:51 +0000

    added alias

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -12,8 +12,12 @@ alias xb='git branch -d'
 alias root='echo "Navigated into the root directory."; cd ~'
 alias dfi='echo "Navigated into the dotfiles directory."; cd ~/developer/dotfiles'
 alias nvc='echo "Navigated into the neovim config directory."; cd ~/.config/nvim'
-alias nv='nvim'
 
 # Sourcing aliases 
 alias .a='echo "Sourced zsh aliases."; source ~/.aliases.zsh'
 alias .z='echo "Sourced .zshrc."; source ~/.zshrc'
+
+# Neovim aliases 
+alias nv='nvim'
+alias ea='nvim ~/.aliases.zsh'
+alias ez='nvim ~/.zshrc'
